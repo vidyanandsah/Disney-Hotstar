bb52a34c023a14a6c2e8f3f5da90d224994b3015
Vidyanand kumar <166861465+vidyanandsah@users.noreply.github.com>
2024-07-14 21:51:02 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Disney-Hotstar-Website-Clone
+The project involved building a Disney+ UI clone entirely from HTML, CSS, and JS.  It is only one page – homepage website. It has a navbar and search box with same effect as Disney+ and it also has a carousel with infinity effect, followed by the movie cards with hover effect. 
---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Cooku with Comali',
+        des: 'The host invites various personalities from the entertainment industry who team up with comedians and prepare delicious dishes to impress the judges.',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'Aashiqana',
+        des: 'Lovers, Karan and Pooja face the biggest bolt from the blue when Poojas plane is hijacked by terrorists. Karan jeopardizes his life to save her only to find out that Poojas brother is one of them.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'Ungalukkaga Oruvan',
+        des: 'A rich businessman returns to India to mend ways with his ex-classmate. However, when he learns about his friends plight, he tries to help him and becomes a saviour for the villagers in the process.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'Total Dhamaal',
+        des: 'When a plane carrying a thiefs treasure crashes in Janakpur, a group of eccentric people and seasoned con artists compete against each other to find it first.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'Neeya Nana',
+        des: 'A talk show where two polarised groups of society are given the platform to share their views and see a different perspective on socially-relevant topics.',
+        image: 'images/slider 5.PNG'
+    }
+]
